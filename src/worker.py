1b7ef951a6bcf2d7f7918d0d6e723c77dc3b7027
Ethan Chang <ehc90000@gmail.com>
2022-05-11 03:10:38 -0500

    updated worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,9 +1,20 @@
 from jobs import q, update_job_status
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+from hotqueue import HotQueue
+import json
+import os
+import redis
 
 @q.worker
 def execute_job(jid):
     update_job_status(jid, 'in progress')
+    
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title("Intensity of Wind Speed in Texas and Oklahoma")
+
     time.sleep(15) # Lol definitely gonna need to replace this
     update_job_status(jid, 'complete')
 
